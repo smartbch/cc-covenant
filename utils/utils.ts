b002207d04e063431454f375e0a5ed7cade0b476
zxh0 <zhangxiuhong2013@gmail.com>
2022-12-21 10:25:25 +0800

    Update utils.ts

--- utils/utils.ts
+++ utils/utils.ts
@@ -28,8 +28,8 @@ export function createElectrumTestnetProvider2() {
   // Initialise a 1-of-2 Electrum Cluster with 2 hardcoded servers
   const electrum = new ElectrumCluster('CashScript Application', '1.4.1', 1, 2, ClusterOrder.PRIORITY);
   electrum.addServer('blackie.c3-soft.com', 60002, ElectrumTransport.TCP_TLS.Scheme, false);
-  // electrum.addServer('tbch.loping.net', 60002, ElectrumTransport.TCP_TLS.Scheme, false);
-  electrum.addServer('testnet.bitcoincash.network', 60002, ElectrumTransport.TCP_TLS.Scheme, false);
+  electrum.addServer('tbch.loping.net', 60002, ElectrumTransport.TCP_TLS.Scheme, false);
+  // electrum.addServer('testnet.bitcoincash.network', 60002, ElectrumTransport.TCP_TLS.Scheme, false);
 
   // Initialise a network provider for network operations on TESTNET
   return new ElectrumNetworkProvider2('testnet3', electrum);
