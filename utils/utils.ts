0871f98a5df56220243bb3871e1b858fcc317bdc
zxh0 <zhangxiuhong2013@gmail.com>
2022-12-21 10:02:33 +0800

    Update utils.ts

--- utils/utils.ts
+++ utils/utils.ts
@@ -12,12 +12,13 @@ import {
 import { ElectrumNetworkProvider } from 'cashscript';
 import ElectrumNetworkProvider2 from './_ElectrumNetworkProvider';
 
+// https://1209k.com/bitcoin-eye/ele.php?chain=tbch
 export function createElectrumTestnetProvider() {
   // Initialise a 1-of-2 Electrum Cluster with 2 hardcoded servers
   const electrum = new ElectrumCluster('CashScript Application', '1.4.1', 1, 2, ClusterOrder.PRIORITY);
   electrum.addServer('blackie.c3-soft.com', 60002, ElectrumTransport.TCP_TLS.Scheme, false);
-  // electrum.addServer('tbch.loping.net', 60002, ElectrumTransport.TCP_TLS.Scheme, false);
-  electrum.addServer('testnet.bitcoincash.network', 60002, ElectrumTransport.TCP_TLS.Scheme, false);
+  electrum.addServer('tbch.loping.net', 60002, ElectrumTransport.TCP_TLS.Scheme, false);
+  // electrum.addServer('testnet.bitcoincash.network', 60002, ElectrumTransport.TCP_TLS.Scheme, false);
 
   // Initialise a network provider for network operations on TESTNET
   return new ElectrumNetworkProvider('testnet3', electrum);
